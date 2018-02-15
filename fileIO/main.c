4334cf764e9e72d7074c28939239e71d57eaf7e7
Jari Mutikainen <jari.mutikainen@coriant.com>
2018-02-15 18:16:49 +0200

    Continued the file IO exercise.

--- fileIO/main.c
+++ fileIO/main.c
@@ -10,15 +10,16 @@
 #include <string.h>
 
 #define MAX 100 // Max length of the full path name of a given file
-#define LINELENGTH 81 // Maximum length of one line in the text file +1 for \0.
+#define MAXLINELENGTH 81 // Maximum length of one line in the text file +1 for \0.
 
 int main(int argc, char *argv[])
 {
+    void tokenizeLine(char* pLine, FILE* ofile);
     FILE *in;
     FILE *out;
-    char iFileName[MAX] =  "/h/GitHubLocalRepository/c-exercises/fileIO/";
-    char oFileName[MAX] =  "/h/GitHubLocalRepository/c-exercises/fileIO/";
-    char *completeLine;
+    char iFileName[MAX] =  "H:\\GitHubLocalRepository\\c-exercises\\fileIO\\";
+    char oFileName[MAX] =  "H:\\GitHubLocalRepository\\c-exercises\\fileIO\\";
+    char *completeLine; // A line of text with '\n' '\0' at the end.
     if(argc != 3) {
         printf("Incorrect number of arguments. Try next time fileIO.exe in.txt out.txt.\n");
         return 1;
@@ -27,8 +28,41 @@ int main(int argc, char *argv[])
     strcat(oFileName,argv[2]); // String concatenation
 
     printf("%s --- %s\n",iFileName,oFileName);
-    completeLine = (char *) malloc(LINELENGTH * sizeof(char));
+    in = fopen(iFileName,"r");
+    if(in == NULL) {
+        printf("Failed to open %s.\n",iFileName);
+    }
+    out = fopen(oFileName,"w");
+    if(out == NULL) {
+        printf("Failed to open %s.\n",oFileName);
+    }
+
+    completeLine = (char *) malloc(MAXLINELENGTH * sizeof(char));
+    if(completeLine == NULL) {
+        printf("malloc() failed to allocate memory for completeLine.\n");
+        exit(1);
+    }
+    // fgets() returns NULL at the end of file.
+    while(fgets(completeLine,MAXLINELENGTH,in)) {
+        fprintf(out, "%s",completeLine);
+        printf("%s",completeLine);
+        tokenizeLine(completeLine,out);
+
+    }
     
     printf("Hello world!\n");
+    fclose(in);
+    fclose(out);
     return 0;
+}
+//------------------------------ tokenizeLine ------------------------------  
+void tokenizeLine(char* pLine, FILE* ofile) {
+    /* This function splits the input line into separate tokens (= words)
+     * using the non-alphanumeric characters as token separators. It prints
+     * these tokens into the output file ofile.
+     */
+    char *p = pLine; // Point at the 1st character of the line.
+    int tokens = 0; // counts the number of the words per line.
+
+    return;
 }
