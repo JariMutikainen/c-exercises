0de742f52740f284043a238f8ba743a72dffc207
Jari Mutikainen <jari.mutikainen@welho.net>
2018-02-17 14:32:25 +0200

    Completed the header-file exercise.

---
+++ p07_02/account.h
@@ -0,0 +1,11 @@
+
+//prototypes declarations
+void initializeAccount();
+void getBalance(void);
+void askCustomer(void);
+void updateAccount(float value);
+void addGift(float giftAmount);
+void thankYou(void);
+
+//Global variables
+float accountBalance, amount;
