1ec2457e2d1bb0568048d19b538a58c2e41a99bd
Jari Mutikainen <jari.mutikainen@welho.net>
2018-03-25 13:26:16 +0300

    Started to work on the state machine exercise.

---
+++ stateMachine/main.c
@@ -0,0 +1,17 @@
+/* This program implements a state machine and simulates the operation
+ * of it. The machine has the following 4 states: S1,S2,S3 and S2B.
+ * The machine has the following 5 inputs: e1,e2,e3,e4 and e5.
+ * The state transition diagram cannot be presented here, but it can be
+ * figured out by reading the code. For the simulation purposes the idea is
+ * to take advantage of a random number generator for producing the input
+ * events.
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
