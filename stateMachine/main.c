dd99d53da7a44452b6ddbf34a4454212608aa6b9
Jari Mutikainen <jari.mutikainen@welho.net>
2018-03-26 09:53:31 +0300

    Completed the state machine exercise.

--- stateMachine/main.c
+++ stateMachine/main.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#define SIMSTEPS 20
 
 typedef struct inputs_s{
     int e1;
@@ -28,10 +28,13 @@ int main()
     void updateState(inputs_t* iV, state_t* s);
     inputs_t iVector;
     state_t state = S1;
+    int i;
 
     srand(time(0)); // Seed the random number generator
-    setInputs(&iVector);
-    updateState(&iVector,&state);
+    for(i=0;i<SIMSTEPS;i++) {
+        setInputs(&iVector);
+        updateState(&iVector,&state);
+    }
 
 
     return 0;
@@ -57,10 +60,30 @@ void updateState(inputs_t* iV, state_t* s) {
     // This function updates the state of the state machine based on the 
     // current state and the inputs.
     
-    char *stateNames[4] = {"S1","S2","SB2","S3"};
+    char *stateNames[4] = {" S1"," S2","SB2"," S3"};
+    state_t oldState = *s; 
+    state_t newState = oldState; 
 
     printf("Inputs: e[5:1]=%1d%1d%1d%1d%1d ",iV->e5,iV->e4,iV->e3,iV->e2,iV->e1);
-    printf("Old state: %s ",stateNames[(int)(*s)]);
+    printf("Old state: %s ",stateNames[(int)(oldState)]);
 
+    switch(oldState) {
+        case S1 :
+            if(iV->e1) newState = S2;
+            else if(iV->e2) newState = S2B;
+            break;
+        case S2:
+            if(iV->e2) newState = S3;
+            else if(iV->e4) newState = S1;
+            break;
+        case S3 :
+            if(iV->e3) newState = S2;
+            break;
+        case S2B :
+            if(iV->e1) newState = S3;
+            break;
+    }
+    printf("New state : %s\n",stateNames[(int)(newState)]);
+    *s = newState;
    return;
 }
