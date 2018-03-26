76f4b13d2557dc72a4082c2e2fe776398cf56b5c
Jari Mutikainen <jari.mutikainen@welho.net>
2018-03-26 19:44:25 +0300

    Started to work on the route comparison exercise.

---
+++ route/main.c
@@ -0,0 +1,17 @@
+/* This program compares the different routes from the home to the office based on
+ * the duration and/or cost. The 3 routes are available are as follows:
+ *
+ * Route A: Train, hourly, $50, 30minutes
+ * Route B: Bus to middle station 20minutes, every 15 minutes + 
+ *          Train to destination 10 minutes every 15 minutes. $25+$30.
+ * Route D: Walk, anytime, 120 minutes. $0.
+ *
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
