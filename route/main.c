7f662e4e181ae4049cb0af206d72bf32d73608d6
Jari Mutikainen <jari.mutikainen@welho.net>
2018-03-27 12:49:25 +0300

    Continued to work on the route selection exercise.

--- route/main.c
+++ route/main.c
@@ -12,6 +12,64 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    void routeA(int startTime, int* delay, int* cost);
+    void routeB(int startTime, int* delay, int* cost);
+    int aDelay,aCost;
+    int bDelay,bCost;
+    int dDelay,dCost;
+    int i = 1;
+
+    routeA(i,&aDelay,&aCost);
+    routeB(i,&bDelay,&bCost);
+
+
+
     return 0;
 }
+//------------------------------ routeA ------------------------------  
+void routeA(int startTime, int* delay, int* cost) {
+    // This function calculates the total travelling time and cost based
+    // on using the route A and starting the commuting startTime minutes
+    // after the hour.
+
+    int initialWait = 60 - startTime;
+    *delay = initialWait + 30;
+    *cost = 50;
+    printf("route A, Train. Start time %d minutes after hour ",startTime);
+    printf("-> total duration = %d minutes, cost = $%d.\n",*delay,*cost);
+    return;
+}
+//------------------------------ routeB ------------------------------  
+void routeB(int startTime, int* delay, int* cost) {
+    // This function calculates the total travelling time and cost based
+    // on using the route B and starting the commuting startTime minutes
+    // after the hour.
+
+    int initialWait;
+    if(startTime <= 15) initialWait = 15 - startTime; 
+    else if(startTime <= 30) initialWait = 30 - startTime; 
+    else if(startTime <= 45) initialWait = 45 - startTime; 
+    else initialWait = 60 - startTime; 
+
+    int arrivalAtMidpoint = startTime + initialWait + 20;
+    if(arrivalAtMidpoint >= 60) {
+        arrivalAtMidpoint -= 60; // substract 1 full hour
+    }
+
+    int trainWait;
+
+
+    if(arrivalAtMidpoint <= 15) trainWait = 15 - arrivalAtMidpoint; 
+    else if(arrivalAtMidpoint <= 30) trainWait = 30 - arrivalAtMidpoint; 
+    else if(arrivalAtMidpoint <= 45) trainWait = 45 - arrivalAtMidpoint; 
+    else trainWait = 60 - arrivalAtMidpoint; 
+
+    *delay = initialWait + 20 + trainWait + 10;
+    *cost = 25 + 30;
+    printf("route B, Bus + Train. Start time %d minutes after hour -> total duration = %d minutes, cost = $%d.\n",
+            startTime,*delay,*cost);
+    return;
+}
+
+    
