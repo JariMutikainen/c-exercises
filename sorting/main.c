bd5409f2e98c03c8b5876bbf6b3209196b371bc3
Jari Mutikainen <jari.mutikainen@coriant.com>
2018-04-05 12:15:45 +0300

    Continued with the sorting algorithms.

--- sorting/main.c
+++ sorting/main.c
@@ -13,6 +13,7 @@ int main()
 {
     void displayNumbers(int* tbl, int elems);
     void selectionSort(int tbl[], int elems, char direction);
+    void insertionSort(int tbl[], int elems, char direction);
     FILE* in;
     in = fopen("numbers.txt","r");
     int num;
@@ -24,9 +25,13 @@ int main()
     }
     printf("The original list of numbers:\n");
     displayNumbers(numbers,elements);
-    selectionSort(numbers,elements,'a');
 
-    printf("After ascending selection sort:\n");
+    selectionSort(numbers,elements,'a');
+    printf("\nAfter ascending selection sort:\n");
+    displayNumbers(numbers,elements);
+
+    insertionSort(numbers,elements,'d');
+    printf("\nAfter decending selection sort:\n");
     displayNumbers(numbers,elements);
 
     printf("Hello world!\n");
@@ -49,37 +54,50 @@ void selectionSort(int tbl[], int elems, char direction) {
     // ni case the direction ='a'. If the direction is 'd' the elements are
     // sorted into the descending order instead.
 
+    int (*selectSwapIndex)(int *t, int lo, int hi); // A function pointer.
     void swap(int *t, int lo, int hi);
-    int selectSwapIndex(int* t, int lo, int hi, char dir);
+    int selectSmallest(int* t, int lo, int hi);
+    int selectLargest(int* t, int lo, int hi);
     int i,indx;
+    if(direction == 'a') { 
+        // For the ascending sorting we need to find the index of the smallest
+        // unsorted element.
+        selectSwapIndex = selectSmallest;
+    } else {
+
+        // For the decending sorting we need to find the index of the largest
+        // unsorted element.
+        selectSwapIndex = selectLargest;
+    }
     for(i=0;i<elems-1;i++) {
-        indx = selectSwapIndex(tbl,i,elems-1,direction);
+        indx = selectSwapIndex(tbl,i,elems-1);
         swap(tbl,i,indx);
     }
     return;
 }
-//------------------------------ selectSwapIndex ------------------------------  
-int selectSwapIndex(int* t, int lo, int hi, char dir) {
-    // When dir == 'a' this function returns the index of the smallest element
+//------------------------------ selectSmallest ------------------------------  
+int selectSmallest(int* t, int lo, int hi) {
+    // This function returns the index of the smallest element
     // of t[] in the range of lo - hi for the purposes of sorting the elements 
-    // into an ascending order. Otherwise this function returns the index of 
-    // the largest element of the array in the range of lo - hi. This is 
-    // used when doing a descending sort.
-    int smallest,largest,k;
-    if(dir == 'a') { // find the index of the smallest element for ascending sort 
-        smallest = lo;
-        for(k=lo+1;k<=hi;k++) {
-            if(t[k] < t[smallest]) smallest = k; // We found a new smallest
-        }
-        return smallest;
-    } else { // Find the index of the largest element for a descending sort
-        largest = lo;
-        for(k=lo+1;k<=hi;k++) {
-            if(t[k] > t[largest]) largest = k; // We found a new largest
-        }
-        return largest;
+    // into an ascending order.
+    int smallest,k;
+    smallest = lo;
+    for(k=lo+1;k<=hi;k++) {
+        if(t[k] < t[smallest]) smallest = k; // We found a new smallest
     }
-    return -1;
+    return smallest;
+}
+//------------------------------ selectLargest  ------------------------------  
+int selectLargest(int* t, int lo, int hi) {
+    // This function returns the index of the largest element
+    // of t[] in the range of lo - hi for the purposes of sorting the elements 
+    // into an decending order.
+    int largest,k;
+    largest = lo;
+    for(k=lo+1;k<=hi;k++) {
+        if(t[k] > t[largest]) largest = k; // We found a new largest
+    }
+    return largest;
 }
 //------------------------------ swap ------------------------------  
 void swap(int *t, int lo, int hi) {
@@ -89,4 +107,22 @@ void swap(int *t, int lo, int hi) {
     *(t+lo) = *(t+hi);
     *(t+hi) = temp;
 }
+//------------------------------ insertionSort ------------------------------  
+void insertionSort(int tbl[], int elems, char direction) {
+    // This function sorts the elements of the tbl[] into the ascending order
+    // ni case the direction ='a'. If the direction is 'd' the elements are
+    // sorted into the descending order instead.
+
+    int (*shiftCondition)(int k,int key,int currElem); // A function pointer
+    int shiftForAscendingSort(int k,int key,int currElem);
+    int shiftForDecendingSort(int k,int key,int currElem);
+    if(direction == 'a') { 
+        // For the ascending sort we need to keep shuffling key left
+        // until it is smaller than the current element.
+        shiftCondition = shiftForAscendingSort;
+    } else {
+        // For the decending sort we need to keep shuffling key left
+        // until it is larger than the current element.
+        shiftCondition = shiftForDecendingSort;
+    }
 
