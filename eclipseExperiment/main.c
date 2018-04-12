485ca3636909751f359a272e52f0a285232054eb
Jari Mutikainen <jari.mutikainen@welho.net>
2018-04-12 17:11:43 +0300

    More experimenting with eclipse.

--- eclipseExperiment/main.c
+++ eclipseExperiment/main.c
@@ -8,7 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    printf("Hello Jari's Eclipse world.\n");
+    int num;
+    printf("Hello Jari's Eclipse world again.\n");
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    printf("Number is: %d.\n",num);
+    getchar();
     return 0;
 }
 
