22eb9f63c2bdc39f47376d0cfe6c4eee457c755f
Jari Mutikainen <jari.mutikainen@coriant.com>
2018-04-12 14:53:25 +0300

    Started to make some experiments using Eclipse IDE.

---
+++ eclipseExperiment/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 12, 2018
+ *      Author: jmutikai
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    printf("Hello Jari's Eclipse world.\n");
+    return 0;
+}
+
